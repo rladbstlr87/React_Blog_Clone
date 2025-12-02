20c756e69122b2dc1cad550cc1b8c2a44572ac5b
rladbstlr87 <manylong@naver.com>
2025-12-02 15:46:07 +0900

    어레이 변수 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '부평 우동 맛집';
-  
+  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '리액트 독학'])
 
   return (
     <div className="App">
@@ -12,7 +13,15 @@ function App() {
         <h4>클로그임</h4>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
 
